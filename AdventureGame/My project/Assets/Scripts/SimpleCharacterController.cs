05d14ba366badfd43574b43efa0197b16b1a3b91
Connor Lawrence <10909920@uvu.edu>
2025-02-07 16:14:54 -0700

    Scripting 4 Lab
    Created platform with sprites, added CharacterController and character art, and added SimpleCharacterController script

---
+++ AdventureGame/My project/Assets/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SimpleCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private CharacterController controller;
+    private Transform thisTransform;
+    private Vector3 movementVector = Vector3.zero;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        thisTransform = transform;
+    }
+
+    private void Update()
+    {
+        MoveCharacter();
+        KeepCharacterOnXAxis();
+    }
+
+    private void MoveCharacter()
+    {
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector *= (moveSpeed * Time.deltaTime);
+        controller.Move(movementVector);
+    }
+
+    private void KeepCharacterOnXAxis()
+    {
+        var currentPosition = transform.position;
+        currentPosition.z = 0f;
+        thisTransform.position = currentPosition;
+    }
+}
