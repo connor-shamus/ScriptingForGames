9e8d871afbbab7d951104daf37f79e3b7dff01c5
Connor Lawrence <10909920@uvu.edu>
2025-03-26 23:47:07 -0600

    Scripting 9 lab
    imported audio clips and added SoundTrigger script to play audio clips on collision

---
+++ AdventureGame/My project/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play(); // Play the attached sound effect
+    }
+
+}
